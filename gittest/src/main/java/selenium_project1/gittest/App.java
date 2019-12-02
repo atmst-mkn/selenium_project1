7604fb3cb05298abd6478726d99919ff27a0fb3f
atmst-mkn <atmst-mkn@users.noreply.github.com>
2019-12-02 16:28:57 +1300

    demo test

---
+++ gittest/src/main/java/selenium_project1/gittest/App.java
@@ -0,0 +1,13 @@
+package selenium_project1.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
